f36d63982b41fd902bda70e81d6e905fc768b56d
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2026-01-12 19:18:10 +0530

    Update and rename gpio_blink.ino to gpio_control_uart_blink.ino

---
+++ basics/gpio_control_uart_blink.ino
@@ -0,0 +1,40 @@
+/*
+* File: gpio_control_uart_blink.ino
+* Platform: ESP32
+* Description:
+*  - Controls LED using UART input
+*  - '1' -> LED ON
+*  - '0' -> LED OFF
+* Concepts:
+*  - GPIO output
+*  - UART serial communication
+*  - Polling using Serial.available() <-- inbuilt object of C++
+*/
+
+
+
+#define LED_PIN 2
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(2000); //2000ms = 2s
+
+  pinMode(LED_PIN,OUTPUT);
+  Serial.println("Type '1' for LED ON / Type '0' for LED_OFF");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available() > 0){   //Polling and Serial.available returns number of bytes
+    char c = Serial.read();
+
+    if(c == '1'){
+      digitalWrite(LED_PIN, HIGH);
+      Serial.println("LED ON");
+    }
+    else if(c == '0'){
+      digitalWrite(LED_PIN, LOW);
+      Serial.println("LED OFF");
+    }
+  }
+}
