49d37b6931962a8a6f61032b87c162e5dde98158
varshithpbs007 <150416730+varshithpbs007@users.noreply.github.com>
2026-02-04 10:47:13 +0530

    Update ESP32_ACCESS_POINT.ino

--- ESP32 Wi-Fi (STA+AP) mode/ESP32_ACCESS_POINT.ino
+++ ESP32 Wi-Fi (STA+AP) mode/ESP32_ACCESS_POINT.ino
@@ -1,1 +1,25 @@
+// - In access point mode, ESP32 creates its own wi-fi hotspot, enabling devices like mobile phones and laptops to connect to it.
+// - No internet or router needed.
+// - It can serve web pages locally.
+// - It works like a mini router.
+// - We obtain the IP addres of ESP32 in AP mode and use it to request for web pages.
 
+#include <WiFi.h>
+
+const char* AP_SSID = "ESP32_Hotspot";
+const char* AP_PASSWORD = "12345678"; // must be 8 charecters long
+void setup()
+{
+  Serial.begin(115200);
+  
+  WiFi.softAP(AP_SSID, AP_PASSWORD); // Starting a WiFi hotspot, ESP32 in Access Point mode, acting as a router
+  Serial.println("Acceess Point Started");
+  Serial.print("IP address:");
+  Serial.println(WiFi.softAPIP()); // This will return the IP address ESP32 assigns itself during hotspot in Access Point mode, which can be used to request the server(ESP32) for webpages
+
+}
+
+void loop()
+{
+  // Nothing here for now
+}
